cce2aed5b7ff98d6708a8f8b38857688117971f3
Théo KOKEL <kokelsan@outlook.com>
2024-03-12 18:47:24 +0100

    Updated event debugger and added base methods for inputs

--- Scripts/Managers/Input/InputManager.cs
+++ Scripts/Managers/Input/InputManager.cs
@@ -1,12 +1,77 @@
-﻿public class InputManager : Manager
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : Manager
 {
+    #region Overrides
+
     protected override void EventHandlerRegister()
+        {
+            
+        }
+    
+        protected override void EventHandlerUnRegister()
+        {
+            
+        }
+
+    #endregion
+
+    #region Mouse & Keyboard
+    
+    public void OnMove(InputValue input)
     {
-        
+        Vector2 value = input.Get<Vector2>();
+        InputManagerHandlerData.Move(value);
+    }
+    
+    public void OnLook(InputValue input)
+    {
+        Vector2 value = input.Get<Vector2>();
+        InputManagerHandlerData.Look(value);
+    }
+    
+    public void OnRightClick()
+    {
+        Vector2 position = Pointer.current.position.value;
+        InputManagerHandlerData.RightClick(position);
+    }
+    
+    public void OnEscape()
+    {
+        InputManagerHandlerData.Escape();
+    }
+    
+    public void OnSpace()
+    {
+        InputManagerHandlerData.Space();
+    }
+    
+    #endregion
+
+    #region Touchscreen
+    
+    public void OnTap()
+    {
+        Vector2 position = Touchscreen.current.primaryTouch.position.value;
+        InputManagerHandlerData.Tap(position);
+    }
+    
+    #endregion
+
+    #region Common
+
+    public void OnPointerMove(InputValue input)
+    {
+        Vector2 value = input.Get<Vector2>();
+        InputManagerHandlerData.PointerMove(value);
+    }
+    
+    public void OnClick()
+    {
+        Vector2 position = Pointer.current.position.value;
+        InputManagerHandlerData.Click(position);
     }
 
-    protected override void EventHandlerUnRegister()
-    {
-        
-    }
+    #endregion
 }
---
+++ Scripts/Utils/EventDebugger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class EventDebugger : Manager
+{
+    public bool DebugSceneLoading;
+    public bool DebugInputs;
+    
+    protected override void EventHandlerRegister()
+    {
+        if (DebugSceneLoading)
+        {
+            SceneLoadingManagerHandlerData.OnSceneLoaded += index => Debug.Log($"SceneLoadingManager: Scene {index} successfully loaded");
+            SceneLoadingManagerHandlerData.OnSceneUnLoaded += index => Debug.Log($"SceneLoadingManager: Scene {index} successfully unloaded");
+        }
+
+        if (DebugInputs)
+        {
+            InputManagerHandlerData.OnMove += _ => Debug.Log($"Moving");
+            InputManagerHandlerData.OnLook += _ => Debug.Log($"Looking");
+            InputManagerHandlerData.OnRightClick += vector2 => Debug.Log($"Right click at {vector2}");
+            InputManagerHandlerData.OnEscape += () => Debug.Log($"Escape pressed");
+            InputManagerHandlerData.OnSpace += () => Debug.Log($"Space pressed");
+            InputManagerHandlerData.OnTap += vector2 => Debug.Log($"Tap at {vector2}");
+            InputManagerHandlerData.OnPointerMove += _ => Debug.Log($"Pointer moving");
+            InputManagerHandlerData.OnClick += vector2 => Debug.Log($"Click at {vector2}");
+        }
+    }
+}
