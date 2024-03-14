06db5ea86d163e2cd227ef5c1c3fc29ea6e3057d
Théo KOKEL <kokelsan@outlook.com>
2024-03-14 20:27:22 +0100

    cleared event debugger

--- Scripts/Utils/EventDebugger.cs
+++ Scripts/Utils/EventDebugger.cs
@@ -4,25 +4,93 @@ public class EventDebugger : Manager
 {
     public bool DebugSceneLoading;
     public bool DebugInputs;
+    public bool DebugGameStates;
     
     protected override void EventHandlerRegister()
     {
         if (DebugSceneLoading)
         {
-            SceneLoadingManagerHandlerData.OnSceneLoaded += index => Debug.Log($"SceneLoadingManager: Scene {index} successfully loaded");
-            SceneLoadingManagerHandlerData.OnSceneUnLoaded += index => Debug.Log($"SceneLoadingManager: Scene {index} successfully unloaded");
+            SceneLoadingManagerHandlerData.OnSceneLoaded += OnSceneLoaded;
+            SceneLoadingManagerHandlerData.OnSceneUnLoaded += OnSceneUnLoaded;
         }
 
         if (DebugInputs)
         {
-            InputManagerHandlerData.OnMove += _ => Debug.Log($"Moving");
-            InputManagerHandlerData.OnLook += _ => Debug.Log($"Looking");
-            InputManagerHandlerData.OnRightClick += vector2 => Debug.Log($"Right click at {vector2}");
-            InputManagerHandlerData.OnEscape += () => Debug.Log($"Escape pressed");
-            InputManagerHandlerData.OnSpace += () => Debug.Log($"Space pressed");
-            InputManagerHandlerData.OnTap += vector2 => Debug.Log($"Tap at {vector2}");
-            InputManagerHandlerData.OnPointerMove += _ => Debug.Log($"Pointer moving");
-            InputManagerHandlerData.OnClick += vector2 => Debug.Log($"Click at {vector2}");
+            InputManagerHandlerData.OnMove += OnMove;
+            InputManagerHandlerData.OnLook += OnLook;
+            InputManagerHandlerData.OnRightClick += OnRightClick;
+            InputManagerHandlerData.OnEscape += OnEscape;
+            InputManagerHandlerData.OnSpace += OnSpace;
+            InputManagerHandlerData.OnTap += OnTap;
+            InputManagerHandlerData.OnPointerMove += OnPointerMove;
+            InputManagerHandlerData.OnClick += OnClick;
+        }
+
+        if (DebugGameStates)
+        {
+            GameManagerManagerHandlerData.OnGameStarted += OnGameStarted;
+            GameManagerManagerHandlerData.OnGamePaused += OnGamePaused;
+            GameManagerManagerHandlerData.OnGameResumed += OnGameResumed;
+            GameManagerManagerHandlerData.OnGameStopped += OnGameStopped;
         }
     }
+    
+    protected override void EventHandlerUnRegister()
+    {
+        if (DebugSceneLoading)
+        {
+            SceneLoadingManagerHandlerData.OnSceneLoaded -= OnSceneLoaded;
+            SceneLoadingManagerHandlerData.OnSceneUnLoaded -= OnSceneUnLoaded;
+        }
+
+        if (DebugInputs)
+        {
+            InputManagerHandlerData.OnMove -= OnMove;
+            InputManagerHandlerData.OnLook -= OnLook;
+            InputManagerHandlerData.OnRightClick -= OnRightClick;
+            InputManagerHandlerData.OnEscape -= OnEscape;
+            InputManagerHandlerData.OnSpace -= OnSpace;
+            InputManagerHandlerData.OnTap -= OnTap;
+            InputManagerHandlerData.OnPointerMove -= OnPointerMove;
+            InputManagerHandlerData.OnClick -= OnClick;
+        }
+
+        if (DebugGameStates)
+        {
+            GameManagerManagerHandlerData.OnGameStarted -= OnGameStarted;
+            GameManagerManagerHandlerData.OnGamePaused -= OnGamePaused;
+            GameManagerManagerHandlerData.OnGameResumed -= OnGameResumed;
+            GameManagerManagerHandlerData.OnGameStopped -= OnGameStopped;
+        }
+    }
+
+    #region Scene Loading
+
+    private void OnSceneLoaded(int index) => Debug.Log($"SceneLoadingManager: Scene {index} successfully loaded");
+    private void OnSceneUnLoaded(int index) => Debug.Log($"SceneLoadingManager: Scene {index} successfully unloaded");
+
+    #endregion
+
+    #region Inputs
+
+    private void OnMove(Vector2 _) => Debug.Log("Moving");
+    private void OnLook(Vector2 _) => Debug.Log("Looking");
+    private void OnRightClick(Vector2 pos) => Debug.Log($"Right click at {pos}");
+    private void OnEscape() => Debug.Log("Escape pressed");
+    private void OnSpace() => Debug.Log("Space pressed");
+    private void OnTap(Vector2 pos) => Debug.Log($"Tap at {pos}");
+    private void OnPointerMove(Vector2 pos) => Debug.Log("Pointer moving");
+    private void OnClick(Vector2 pos) => Debug.Log($"Click at {pos}");
+
+    #endregion
+
+    #region Game states
+
+    private void OnGameStarted() => Debug.Log("Game started");
+    private void OnGamePaused() => Debug.Log("Game paused");
+    private void OnGameResumed() => Debug.Log("Game resumed");
+    private void OnGameStopped() => Debug.Log("Game stopped");
+
+    #endregion
+    
 }
