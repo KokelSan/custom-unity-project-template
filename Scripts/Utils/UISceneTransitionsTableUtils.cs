af90a35f0323e308d5b83f8d0c4901b1b0a91c1b
Théo KOKEL <kokelsan@outlook.com>
2024-03-22 18:23:46 +0100

    Removed debugs

--- Scripts/Utils/UISceneTransitionsTableUtils.cs
+++ Scripts/Utils/UISceneTransitionsTableUtils.cs
@@ -1,32 +1,24 @@
 ﻿using System.Collections.Generic;
-using UnityEngine;
 
 public static class UISceneTransitionsTableUtils
 {
         public static ScreenTransitionType GetTransition(UISceneTransitionsTableSO table, int from, int to)
         {
-                // specific transition
                 if (TryGetSpecificTransition(table.SpecificTransitions, from, to, out ScreenTransitionType transitionType))
                 {
-                        Debug.Log($"Transition from {from} to {to} is {transitionType}");
                         return transitionType;
                 }
                 
-                // global FROM transition
                 if (TryGetGlobalTransition(table.GlobalFromTransitions, from, out transitionType))
                 {
-                        Debug.Log($"Transition from {from} is {transitionType}");
                         return transitionType;
                 }
                 
-                //  global TO transition
                 if (TryGetGlobalTransition(table.GlobalToTransitions, to, out transitionType))
                 {
-                        Debug.Log($"Transition to {to} is {transitionType}");
                         return transitionType;
                 }
 
-                Debug.Log($"Default transition ({table.DefaultTransition}) applied from {from} to {to}");
                 return table.DefaultTransition;
         }
         
