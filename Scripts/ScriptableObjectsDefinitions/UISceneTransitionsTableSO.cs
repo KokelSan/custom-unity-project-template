af567165779c7e70b19a2e186b67a8e828e181f9
Théo KOKEL <kokelsan@outlook.com>
2024-03-22 16:15:08 +0100

    Added scene transition table scriptable objects (no use yet)

---
+++ Scripts/ScriptableObjectsDefinitions/UISceneTransitionsTableSO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public struct SpecificTransition
+{
+        public int FromSceneIndex;
+        public int ToSceneIndex;
+        public ScreenTransitionType TransitionType;
+}
+
+[Serializable]
+public struct GlobalTransition
+{
+        public int SceneIndex;
+        public ScreenTransitionType TransitionType;
+}
+
+[CreateAssetMenu(menuName = "Project Template/New UI Scene Transitions Table",  fileName = "UISceneTransitionsTableSO")]
+public class UISceneTransitionsTableSO : ScriptableObject
+{
+        public ScreenTransitionType DefaultTransition;
+        public List<SpecificTransition> SpecificTransitions;
+        public List<GlobalTransition> GlobalFromTransitions;
+        public List<GlobalTransition> GlobalToTransitions;
+}
---
+++ Scripts/Utils/UISceneTransitionsTableUtils.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+
+public static class UISceneTransitionsTableUtils
+{
+        public static ScreenTransitionType GetTransition(UISceneTransitionsTableSO table, int from = -1, int to = -1)
+        {
+                // specific transition
+                if (from >= 0 && to >= 0)
+                {
+                        if (TryGetSpecificTransition(table.SpecificTransitions, from, to, out ScreenTransitionType transitionType))
+                        {
+                                return transitionType;
+                        }
+                }
+                
+                // global FROM transition
+                else if (from >= 0)
+                {
+                        if (TryGetGlobalTransition(table.GlobalFromTransitions, from, out ScreenTransitionType transitionType))
+                        {
+                                return transitionType;
+                        }
+                }
+                
+                //  global TO transition
+                else
+                {
+                        if (TryGetGlobalTransition(table.GlobalToTransitions, to, out ScreenTransitionType transitionType))
+                        {
+                                return transitionType;
+                        }
+                }
+
+                return table.DefaultTransition;
+        }
+        
+        private static bool TryGetSpecificTransition(List<SpecificTransition> transitions, int from, int to, out ScreenTransitionType transitionType)
+        {
+                SpecificTransition transition = transitions.Find(transition => transition.FromSceneIndex == from && transition.ToSceneIndex == to);
+                if (transition.Equals(default))
+                {
+                        transitionType = default;
+                        return false;
+                }
+                
+                transitionType = transition.TransitionType;
+                return true;
+        }
+
+        private static bool TryGetGlobalTransition(List<GlobalTransition> transitions, int sceneIndex, out ScreenTransitionType transitionType)
+        {
+                GlobalTransition transition = transitions.Find(transition => transition.SceneIndex == sceneIndex);
+                if (transition.Equals(default))
+                {
+                        transitionType = default;
+                        return false;
+                }
+                
+                transitionType = transition.TransitionType;
+                return true;
+        }
+}
